038db6077e03ac5e484f57cc591044d42ce45bee
danielperezflorit <daniel.perez.florit@estudiantat.upc,edu>
2024-10-22 01:22:30 +0200

    Activitat feta

---
+++ src/routes/user.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import { getUsers, createUser, getUser, updateUser, deleteUser, login, profile } from '../controllers/userController';
+import { TokenValidation} from '../middleware/verifyJWT'
+import { verifyOwnership } from '../middleware/verifyOwner'
+import { AdminValidation} from '../middleware/verifyAdmin'
+const router = express.Router();
+
+// Ruta para obtener todos los usuarios
+//router.get("/", getUsers);
+// Ruta per obtenir tots els usuaris (només per administradors)
+
+router.get("/", TokenValidation, AdminValidation, getUsers);
+
+
+
+// Ruta per crear usuari
+router.post("/", createUser);
+
+//Ruta per obtenir usuari per id
+router.get("/:id", getUser);
+
+
+
+//Ruta per actialitzar usuari per id
+//router.put("/update/:id", updateUser);
+
+// Ruta para actualizar un usuario por su ID (solo el propietario o admin puede hacerlo)
+router.put("/update/:id", TokenValidation, verifyOwnership, updateUser);
+
+
+
+//Ruta per eliminar user per id
+router.delete('/delete/:id', TokenValidation, AdminValidation, deleteUser);
+
+//Ruta per fer login
+router.post("/login", login);
+
+//Ruta per veure el perfil amb token
+router.get("/:id/profile", TokenValidation, verifyOwnership, profile);
+
+
+
+
+export default router 
